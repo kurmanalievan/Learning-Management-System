b76d5fabc927953d42d703a9a81b9ec89bf74d67
nursezim <kurmanalieva.nurs@gmail.com>
2023-04-23 02:53:28 +0200

    Add seeder for generating random data.

--- database/seeders/SubjectSeeder.php
+++ database/seeders/SubjectSeeder.php
@@ -2,8 +2,11 @@
 
 namespace Database\Seeders;
 
+use App\Models\Subject;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
 
 class SubjectSeeder extends Seeder
 {
@@ -12,6 +15,7 @@ class SubjectSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+       DB::table('subjects')->truncate();
+       Subject::factory()->count(10)->create();
     }
 }
